cca7503df7023c4a24820ed6dd8742209037117c
Bobi <bbintev@gmail.com>
2025-07-16 16:41:03 +0300

    Fix duplicate builder in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Добави Razor Pages
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Вземи порта от Render (или използвай 1000 по подразбиране)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "1000";
+app.Urls.Add($"http://*:{port}");
+
+// Конфигурирай pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
 
 app.Run();
